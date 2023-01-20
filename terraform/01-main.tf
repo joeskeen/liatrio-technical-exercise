7ac19e5d901d25ba1c667406446bd302f75b21b4
Joe Skeen <joeskeen@outlook.com>
2023-01-20 11:03:17 -0700

    fix vpc module issues in tf file

--- terraform/01-main.tf
+++ terraform/01-main.tf
@@ -95,26 +95,30 @@ resource "aws_security_group" "all_worker_mgmt" {
 }
 
 module "vpc" {
-  source              = "terraform-aws-modules/vpc/aws"
-  version             = "3.19.0"
-  name                = "${var.cluster-name}-vpc"
-  cidr                = "10.0.0.0/16"
-  azs                 = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway  = true
-  single_nat_gateway  = true
-  tags                = merge(local.tags, map("kubernetes.io/cluster/${local.cluster_name}", "shared", "kubernetes.io/role/internal-elb", "", "kubernetes.io/role/elb", ""))
-  public_subnet_tags  = merge(map("kubernetes.io/role/elb", "1"))
-  private_subnet_tags = merge(map("kubernetes.io/role/internal-elb", "1"))
+  source             = "terraform-aws-modules/vpc/aws"
+  version            = "3.19.0"
+  name               = "${var.cluster-name}-vpc"
+  cidr               = "10.0.0.0/16"
+  azs                = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
+  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  tags = merge(local.tags, tomap({
+    "kubernetes.io/cluster/${local.cluster_name}" : "shared",
+    "kubernetes.io/role/internal-elb" : "",
+    "kubernetes.io/role/elb" : ""
+  }))
+  public_subnet_tags  = merge(tomap({ "kubernetes.io/role/elb" : "1" }))
+  private_subnet_tags = merge(tomap({ "kubernetes.io/role/internal-elb" : "1" }))
 }
 
 module "eks" {
-  source                             = "terraform-aws-modules/eks/aws"
-  cluster_name                       = local.cluster_name
-  subnet_ids                         = ["${module.vpc.private_subnets}"]
-  tags                               = local.tags
-  vpc_id                             = module.vpc.vpc_id
-  eks_managed_node_groups            = local.eks_managed_node_groups
-  node_security_group_id             = aws_security_group.all_worker_mgmt.id
+  source                  = "terraform-aws-modules/eks/aws"
+  cluster_name            = local.cluster_name
+  subnet_ids              = ["${module.vpc.private_subnets}"]
+  tags                    = local.tags
+  vpc_id                  = module.vpc.vpc_id
+  eks_managed_node_groups = local.eks_managed_node_groups
+  node_security_group_id  = aws_security_group.all_worker_mgmt.id
 }
